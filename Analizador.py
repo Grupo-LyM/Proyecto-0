624af6c5df933e80786ad96815dde46a93e0ec77
Sebastian Casanova <s.casanovao@uniandes.edu.co>
2022-02-20 19:39:56 -0500

    Avance

--- Analizador.py
+++ Analizador.py
@@ -1,7 +1,10 @@
 def menu():
     print("Bienvenido a el analisador.")
-    ruta = input("\n Escriba la ruta del archivo txt del cual quieras conocer si es correcto o no ( de la forma C:\Users\elfue\Documents\Proyecto 0 LyM\ejemplo.txt ): ")
-    archivo = open(ruta, "r")
+    ruta = input("Escriba la ruta ESPECIFICA del archivo :")
+    archivo = open(str(ruta),"r")
     lineas = archivo.readlines()
     archivo.close()
 
+    print(lineas)
+
+menu()
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Realizado por:
 Ana Sofia Castellanos 202114167
 Sebastian Casanova Ospina 202115116
+
+Nota:
+En el apartado de ruta, esta debe ser escrita de la forma "C:\Users\elfue\Documents\Proyecto 0 LyM\Proyecto-0\Ejemplo.txt"
