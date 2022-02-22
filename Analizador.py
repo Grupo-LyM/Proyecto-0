9a95fee0f88007809ab8c06f1354b7ea4ec2f534
acastem15 <a.castellanosm@uniandes.edu.co>
2022-02-21 22:19:21 -0500

    condiciones

--- Analizador.py
+++ Analizador.py
@@ -225,6 +225,7 @@ def validarDs(lst:str):
         list_dir = lst.split(" ")
         print(lst)
         for dir in  list_dir:
+            
             val_dir = validarCombFr_Ba_Ri_Le(dir)
             if val_dir==False:
                 return False
@@ -232,7 +233,6 @@ def validarDs(lst:str):
     else:
         return False
     
-
 
 #Validacion de comandos
 
@@ -359,7 +359,7 @@ def validarRun_dirs(list_comando:str)->bool:
         # Revisa si lo siguiente es lista Ds
         list_comando.remove(list_comando[0])
         elemDs=list_comando
-        formatDs = "("
+        formatDs = ""
         i=0
         for e in elemDs: 
             if i ==len(elemDs)-1:
@@ -367,7 +367,7 @@ def validarRun_dirs(list_comando:str)->bool:
             else: 
                 formatDs+=(e)+" "
             i+=1
-        formatDs+=")"
+        formatDs+=""
       
         print(formatDs)
         val1= validarDs(formatDs)
@@ -398,8 +398,9 @@ def validarComando(comando:str):
     #Validacion de parentesis de abrir y cerrar
     if (comando[0] is alfabeto["par1"]) and (comando[-1]is alfabeto["par2"]):
         #Se eliminan los paréntesis de la instruccion
-        comando = comando.replace(comando[0],"")
-        comando = comando.replace(comando[-1],"")
+        comando = comando[1:] 
+        comando = comando[:len(comando)-1]
+        
         #Se divide la parte interna por " "-> [palabra,combinacion]
         list_comando = comando.split(" ")
         print("   Lista")
@@ -521,20 +522,133 @@ def validarFacing_p(list_condicion)->bool:
     else:
         return False
 
+def validarcan_put_p(list_condicion)->bool:
+    if list_condicion[0]==alfabeto["condiciones"][1]:
+    # Revisa si lo siguiente es cardinal
+        format = list_condicion[1].replace(" ","")+" "+list_condicion[2].replace(" ","")
+        val1= validarCombBC_NamNum(format)
+        if (val1==True):
+            return True
+        else: 
+            return False
+    else:
+        return False
+def validarcan_pick_p(list_condicion)->bool:
+    if list_condicion[0]==alfabeto["condiciones"][2]:
+    # Revisa si lo siguiente es Bal/ chip cardinal
+        format = list_condicion[1].replace(" ","")+" "+list_condicion[2].replace(" ","")
+        val1= validarCombBC_NamNum(format)
+        if (val1==True):
+            return True
+        else: 
+            return False
+    else:
+        return False
+
+def validarcan_move_p(list_condicion)->bool:
+    if list_condicion[0]==alfabeto["condiciones"][3]:
+    # Revisa si lo siguiente es Bal/ chip cardinal
+        
+        val1= validarCardinales(list_condicion[1])
+        if (val1==True):
+            return True
+        else: 
+            return False
+    else:
+        return False
+def validarnot(list_condicion)->bool:
+    if list_condicion[0]==alfabeto["condiciones"][4]:
+        list_condicion.remove(list_condicion[0])
+        elemC=list_condicion
+        formatC = ""
+        i=0
+        for e in elemC: 
+            if i ==len(elemC)-1:
+                formatC+=e
+            else: 
+                formatC+=(e)+" "
+            i+=1
+        formatC+=""
+    # Revisa si lo siguiente es cardinal
+        val1= validarCondicion(formatC)
+        if (val1==True):
+            return True
+        else: 
+            return False
+    else:
+        return False
+
 #Validacion junto con parentesis
 def validarCondicion(condicion:str):
    
-
+    ok=True
     #Validacion de parentesis de abrir y cerrar
     if (condicion[0] is alfabeto["par1"]) and (condicion[-1]is alfabeto["par2"]):
-
+        
         #Se eliminan los paréntesis de la instruccion
-        condicion = condicion.replace(condicion[0],"")
-        condicion = condicion.replace(condicion[-1],"")
+        condicion = condicion[1:] 
+        condicion = condicion[:len(condicion)-1]
+        
+        print(condicion)
         #Se divide la parte interna por " "-> [palabra,combinacion]
-        list_condicion = condicion.split(" ")
+        lst_condicion = condicion.split(" ")
+        print("condddd")
+        print(lst_condicion)
+        print("condddd")
 
-        validarFacing_p(list_condicion)
+        if  len(lst_condicion)==2:
+
+            #Facing_p
+            cond1=validarFacing_p(lst_condicion)
+            if cond1==True:
+                print("Si")
+                return True
+            else:
+                ok=False
+            #can_move_p
+            cond2=validarcan_move_p(lst_condicion)
+            if cond2==True:
+                print("Si")
+                return True
+            else:
+                ok=False
+
+            
+
+        if  len(lst_condicion)==3:
+            cond4=validarcan_put_p(lst_condicion)
+            if cond4==True:
+                print("Si")
+                return True
+            else:
+                ok=False
+
+            cond5=validarcan_pick_p(lst_condicion)
+            if cond5==True:
+                print("Si")
+                return True
+            else:
+                ok=False
+        ok=False
+        if  len(lst_condicion)>=2:
+
+            cond3=validarnot(lst_condicion)
+            
+                
+            if cond3==True:
+                print("Si")
+                return True
+            else:
+                ok=False
+
+    else: 
+        ok=False
+    return ok
+
+
+
+
+
 
 #####Pruebas
 def probarparentesis(comando:str)->bool:
@@ -552,7 +666,7 @@ def probarcomandos():
         
         e = e.replace("\n","")
         print("yaaaaa")
-        print(validarComando(e))
+        print(validarCondicion(e))
         
 
 probarcomandos()
